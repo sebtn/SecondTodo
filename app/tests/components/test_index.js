406136525a3d5388183c5aa6893b8464047cdc22
sebtn <sdebedout@gmail.com>
2017-06-09 01:07:22 -0400

    New test added for each todo in array todoList

--- app/tests/components/test_index.js
+++ app/tests/components/test_index.js
@@ -44,6 +44,17 @@ describe('Component TodoList', () => {
 		expect(TodoList).toExist()
 	})
 
+		it('Test#2: should render one Todo for each item in TodoList array', () => {
+			let todos = [
+				{id:1, text:'Some text for item 1'},
+				{id:2, text:'some text for item 2' }
+			]
+			/*from dom */
+			let todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />)
+			let todosRendered = TestUtils.scryRenderedComponentsWithType(todoList, Todo)
+
+			expect(todos.length).toBe(todosRendered.length)
+	})
 
 })
 
